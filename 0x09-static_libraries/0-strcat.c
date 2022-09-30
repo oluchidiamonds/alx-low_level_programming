b96ad34dd877c3e3ca8c1da97f945a9077a9abbb
oluchidiamonds <oluchidiamonds7@gmail.com>
2022-09-30 11:42:07 -0700

    task 99

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: The destination string
+ * @src: The source string
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, n;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+	}
+	for (n = 0; (dest[i + n] = *src++) != '\0'; n++)
+	{
+	}
+	return (dest);
+}
